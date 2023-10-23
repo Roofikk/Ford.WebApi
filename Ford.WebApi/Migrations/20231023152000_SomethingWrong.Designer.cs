85ac74e4095f8ce357763ff59f0d0ae87f9489f1
Roofikk <rufik956@gmail.com>
2023-10-23 18:43:40 +0300

    Починил регистрацию

--- Ford.WebApi/Migrations/20231023152000_SomethingWrong.Designer.cs
+++ Ford.WebApi/Migrations/20231023152000_SomethingWrong.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Ford.WebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ford.WebApi.Migrations
 {
     [DbContext(typeof(FordContext))]
-    partial class FordContextModelSnapshot : ModelSnapshot
+    [Migration("20231023152000_SomethingWrong")]
+    partial class SomethingWrong
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.23");
---
+++ Ford.WebApi/Migrations/20231023152000_SomethingWrong.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ford.WebApi.Migrations
+{
+    public partial class SomethingWrong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdatedDate",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDate",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdatedDate",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDate",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "AspNetUsers",
+                type: "nvarchar(24)",
+                nullable: true);
+        }
+    }
+}
